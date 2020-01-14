e6701093bca1465e0a265eb40c0d10a5c5849652
Stevan Cuk <stevancuk@yahoo.com>
2020-01-14 19:55:21 +0100

    Starting html

--- js/js-v1.js
+++ js/js-v1.js
@@ -7,3 +7,4 @@ $(function(){
 
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! NOTES - QUESTIONS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+// * Do you want length input to be a drop-down select (<1.25m, 1.25m-5m, 5m-6m ....)
