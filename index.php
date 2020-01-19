7106dfd042c2a8a4d638bd13af71b86767e21647
Stevan Cuk <stevancuk@yahoo.com>
2020-01-19 17:02:16 +0100

    all jsons made

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php header('Access-Control-Allow-Origin: *'); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,20 +17,20 @@
 					<div id="postcode_wrap">
 						<label for="carr_input_postcode"> Your Postcode: </label>
 						<input type="text" id="carr_input_postcode" value="LL58 3PZ ">
-						<i class="far fa-question-circle"></i>
+						<i class="far fa-question-circle carrInfos"></i>
 						<p id="post_error_info"><i class="fas fa-exclamation-circle"></i> Postcode input error</p>
 					</div>
 
 					<div id="pipe_wrap">
 						<label for="carr_input_pipe"> Your Pipe: </label>
 						<input type="number" id="carr_input_pipe" value="2.5">
-						<i class="far fa-question-circle"></i>
+						<i class="far fa-question-circle carrInfos"></i>
 					</div>
 
 					<div id="address_wrap">
 						<label for="carr_input_address"> Your Address: </label>
-						<input type="text" id="carr_input_address" value="Mil Obr 12">
-						<i class="far fa-question-circle"></i>
+						<input type="text" id="carr_input_address" value="Abbey Road 12">
+						<i class="far fa-question-circle carrInfos"></i>
 					</div>
 
 				</div>
@@ -45,19 +46,19 @@
 
 				<div id="tuff_headlines">
 					<div id="tuff_head1">
-						<p> Area </p>
+						<p class="boldHead"> Area </p>
 						<p id="tuff_result1"> Lon </p>
 					</div>
 					<div id="tuff_head2">
-						<p> Area Standing Charge </p>
+						<p class="boldHead"> Area Standing Charge </p>
 						<p id="tuff_result2"> £<span>7.09</span> </p>
 					</div>
 					<div id="tuff_head3">
-						<p id="tuff_pipe_length"> !!Dinamically text length!!! </p>
+						<p class="boldHead" id="tuff_pipe_length"> !!Dinamically text length!!! </p>
 						<p id="tuff_result3"> £<span>4.95</span> </p>
 					</div>
 					<div id="tuff_head4">
-						<p> Residential Supplement </p>
+						<p class="boldHead"> Residential Supplement </p>
 						<p id="tuff_result4"> £<span>5.57</span> </p>
 					</div>					
 				</div>
@@ -66,23 +67,23 @@
 
 				<div id="tuff_supp_headlines">
 					<div id="tuff_supp_head1">
-						<p> 2-3 Day Delivery </p>
+						<p class="boldHead"> 2-3 Day Delivery </p>
 						<p id="tuff_supp_result1"> £<span>-1.50</span> </p>
 					</div>
 					<div id="tuff_supp_head2">
-						<p> Next Working Day Delivery </p>
+						<p class="boldHead"> Next Working Day Delivery </p>
 						<p id="tuff_supp_result2"> £<span>0.00</span> </p>
 					</div>
 					<div id="tuff_supp_head3">
-						<p> AM Delivery </p>
+						<p class="boldHead"> AM Delivery </p>
 						<p id="tuff_supp_result3"> £<span>6.13</span> </p>
 					</div>
 					<div id="tuff_supp_head4">
-						<p> Pre 10:30am Delivery </p>
+						<p class="boldHead"> Pre 10:30am Delivery </p>
 						<p id="tuff_supp_result4"> £<span>23.37</span> </p>
 					</div>
 					<div id="tuff_supp_head5">
-						<p> Saturday Delivery </p>
+						<p class="boldHead"> Saturday Delivery </p>
 						<p id="tuff_supp_result5"> £<span>24.48</span> </p>
 					</div>					
 				</div>
@@ -90,8 +91,53 @@
 			</div>
 
 			<div id="carr_dhl_wrap">
+				<h3> DHL </h3>
 
-			</div>
+				<div id="dhl_headlines">
+					<div id="dhl_head1">
+						<p class="boldHead"> Zone </p>
+						<p id="dhl_result1"> A </p>
+					</div>
+					<div id="dhl_head2">
+						<p class="boldHead"> Zone Standing Charge </p>
+						<p id="dhl_result2"> £<span>7.09</span> </p>
+					</div>
+					<div id="dhl_head3">
+						<p class="boldHead" id="dhl_pipe_length"> No Pipe/1.25m Lengths </p>
+						<p id="dhl_result3"> £<span>0.00</span> </p>
+					</div>
+					<div id="dhl_head4">
+						<p class="boldHead"> Lengths Above 1.25m </p>
+						<p id="dhl_result4"> Pipe must be cut to 1.25m </p>
+					</div>					
+				</div>
+
+				<h4> DHL Shipping Service Supplements </h4>
+
+				<div id="dhl_supp_headlines">
+					<div id="dhl_supp_head1">
+						<p class="boldHead"> 2-3 Day Delivery </p>
+						<p id="dhl_supp_result1"> £<span>-1.50</span> </p>
+					</div>
+					<div id="dhl_supp_head2">
+						<p class="boldHead"> Next Working Day Delivery </p>
+						<p id="dhl_supp_result2"> £<span>0.00</span> </p>
+					</div>
+					<div id="dhl_supp_head3">
+						<p class="boldHead"> AM Delivery </p>
+						<p id="dhl_supp_result3"> £<span>4.16</span> </p>
+					</div>
+					<div id="dhl_supp_head4">
+						<p class="boldHead"> Pre 10:30am Delivery </p>
+						<p id="dhl_supp_result4"> £<span>8.29</span> </p>
+					</div>
+					<div id="dhl_supp_head5">
+						<p class="boldHead"> Saturday Delivery </p>
+						<p id="dhl_supp_result5"> £<span>10.37</span> </p>
+					</div>					
+				</div>
+
+			</div>	
 
 		</div>
 
